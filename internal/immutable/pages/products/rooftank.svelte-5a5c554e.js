1de19fe60132aca15752b8c7f740265d62302067
Ben Wu <benayaj.2004@gmail.com>
2022-06-23 15:50:36 +0700

    Updates

---
+++ internal/immutable/pages/products/rooftank.svelte-5a5c554e.js
@@ -0,0 +1,39 @@
+import{S as ha,i as pa,s as ba,e,t as r,k,c as i,a as u,h as s,d as l,m,b as ta,g as ca,F as a,n as ea}from"../../chunks/index-f1db066b.js";function Pa(oa){let n,f,T,F,y,I,j,d,w,H,L,M,z,J,p,o,N,V,U,D,v,G,K,R,q,C,g,_,O,Q,E,W,X,S,Y,Z,h,$,b,aa,na;return{c(){n=e("div"),f=e("h2"),T=r("FRP Roof Tank"),F=k(),y=e("p"),I=r(`Tangki Air Panel FRP atau sering disebut dengan nama Roof Tank adalah Tangki penyimpanan air\r
+		bersih yang biasa dipasang di atas sebuah Bangunan umum, baik itu hotel/apartement, pusat\r
+		perbelanjaan (mall), gedung Perkantoran, Rumah Sakit, sekolah/universitas, pabrik dan bangunan\r
+		umum lainnya.`),j=k(),d=e("p"),w=r(`FRP sendiri adalah singkatan dari Fiber Reinforced Plastic yaitu material komposit yang terdiri\r
+		dari serat fiber dan matriks resin yang lebih dikenal dengan sebutan FIBERGLASS.\r
+		`),H=e("br"),L=r(`\r
+		Pada umumnya Tangki Air Panel ini dirakit dengan sistem Knock down dari Panel2 dengan ukuran 100\r
+		x 100 cm atau 100 x 50 cm yang disusun dan diikat dengan menggunakan baut di bagian luar serta diperkuat\r
+		dengan struktur rangka besi anti karat di bagian dalam kemudian dirangkai sesuai dengan ukuran dan\r
+		kapasitas yang dibutuhkan.\r
+		`),M=e("br"),z=r(`\r
+		Tangki Air Panel ini dibuat sesuai dengan pesanan (bukan unit jadi) dengan satuan kubikasi (m3) yang\r
+		setara dengan 1000 liter. Harga dihitung sesuai volume (volume semakin besar, harga semakin murah).`),J=k(),p=e("div"),o=e("p"),N=r(`Berhubung barang diproduksi di Jakarta, maka pesanan dengan cara indent paling lama 20 hari\r
+			dari tanggal pemesanan. Untuk pemasangan dari kami akan dikenakan biaya pasang.\r
+			`),V=e("br"),U=r(`\r
+			Pemesanan unit sudah termasuk aksesoris seperti: External & Internal ladder, Roof rim, Manhole\r
+			Panel, Air Ventilator, Roof Panel, Bolt nut washer, Nozzle, Anchor set & Pit Panel.`),D=k(),v=e("p"),G=r(`Standar kami dalam merakit unit menggunakan sekrup & mur dari material Stainless Steel sehingga\r
+		awet dan tahan karat. Proses pemasangan atau perakitan dilakukan oleh tenaga ahli yang\r
+		berpengalaman untuk menjamin hasil pemasangan yang rapih, tidak bocor dan sanggup bertahan\r
+		sampai dengan waktu yang sangat lama.`),K=k(),R=e("p"),q=r("Bangunan yang menggunakan produk kami di Batam, antara lain:"),C=k(),g=e("ul"),_=e("li"),O=r("PT JMS Batamindo"),Q=k(),E=e("li"),W=r("Nagoya Hill Mall"),X=k(),S=e("li"),Y=r("Apartemen Pollux Habibie"),Z=k(),h=e("p"),$=r("Silahkan "),b=e("a"),aa=r("hubungi kami"),na=r(" untuk informasi lebih lanjut."),this.h()},l(B){n=i(B,"DIV",{class:!0});var t=u(n);f=i(t,"H2",{});var ia=u(f);T=s(ia,"FRP Roof Tank"),ia.forEach(l),F=m(t),y=i(t,"P",{});var ra=u(y);I=s(ra,`Tangki Air Panel FRP atau sering disebut dengan nama Roof Tank adalah Tangki penyimpanan air\r
+		bersih yang biasa dipasang di atas sebuah Bangunan umum, baik itu hotel/apartement, pusat\r
+		perbelanjaan (mall), gedung Perkantoran, Rumah Sakit, sekolah/universitas, pabrik dan bangunan\r
+		umum lainnya.`),ra.forEach(l),j=m(t),d=i(t,"P",{});var c=u(d);w=s(c,`FRP sendiri adalah singkatan dari Fiber Reinforced Plastic yaitu material komposit yang terdiri\r
+		dari serat fiber dan matriks resin yang lebih dikenal dengan sebutan FIBERGLASS.\r
+		`),H=i(c,"BR",{}),L=s(c,`\r
+		Pada umumnya Tangki Air Panel ini dirakit dengan sistem Knock down dari Panel2 dengan ukuran 100\r
+		x 100 cm atau 100 x 50 cm yang disusun dan diikat dengan menggunakan baut di bagian luar serta diperkuat\r
+		dengan struktur rangka besi anti karat di bagian dalam kemudian dirangkai sesuai dengan ukuran dan\r
+		kapasitas yang dibutuhkan.\r
+		`),M=i(c,"BR",{}),z=s(c,`\r
+		Tangki Air Panel ini dibuat sesuai dengan pesanan (bukan unit jadi) dengan satuan kubikasi (m3) yang\r
+		setara dengan 1000 liter. Harga dihitung sesuai volume (volume semakin besar, harga semakin murah).`),c.forEach(l),J=m(t),p=i(t,"DIV",{class:!0});var sa=u(p);o=i(sa,"P",{});var A=u(o);N=s(A,`Berhubung barang diproduksi di Jakarta, maka pesanan dengan cara indent paling lama 20 hari\r
+			dari tanggal pemesanan. Untuk pemasangan dari kami akan dikenakan biaya pasang.\r
+			`),V=i(A,"BR",{}),U=s(A,`\r
+			Pemesanan unit sudah termasuk aksesoris seperti: External & Internal ladder, Roof rim, Manhole\r
+			Panel, Air Ventilator, Roof Panel, Bolt nut washer, Nozzle, Anchor set & Pit Panel.`),A.forEach(l),sa.forEach(l),D=m(t),v=i(t,"P",{});var la=u(v);G=s(la,`Standar kami dalam merakit unit menggunakan sekrup & mur dari material Stainless Steel sehingga\r
+		awet dan tahan karat. Proses pemasangan atau perakitan dilakukan oleh tenaga ahli yang\r
+		berpengalaman untuk menjamin hasil pemasangan yang rapih, tidak bocor dan sanggup bertahan\r
+		sampai dengan waktu yang sangat lama.`),la.forEach(l),K=m(t),R=i(t,"P",{});var ua=u(R);q=s(ua,"Bangunan yang menggunakan produk kami di Batam, antara lain:"),ua.forEach(l),C=m(t),g=i(t,"UL",{});var P=u(g);_=i(P,"LI",{});var da=u(_);O=s(da,"PT JMS Batamindo"),da.forEach(l),Q=m(P),E=i(P,"LI",{});var ga=u(E);W=s(ga,"Nagoya Hill Mall"),ga.forEach(l),X=m(P),S=i(P,"LI",{});var ka=u(S);Y=s(ka,"Apartemen Pollux Habibie"),ka.forEach(l),P.forEach(l),Z=m(t),h=i(t,"P",{});var x=u(h);$=s(x,"Silahkan "),b=i(x,"A",{href:!0});var ma=u(b);aa=s(ma,"hubungi kami"),ma.forEach(l),na=s(x," untuk informasi lebih lanjut."),x.forEach(l),t.forEach(l),this.h()},h(){ta(p,"class","box"),ta(b,"href","/contact"),ta(n,"class","container")},m(B,t){ca(B,n,t),a(n,f),a(f,T),a(n,F),a(n,y),a(y,I),a(n,j),a(n,d),a(d,w),a(d,H),a(d,L),a(d,M),a(d,z),a(n,J),a(n,p),a(p,o),a(o,N),a(o,V),a(o,U),a(n,D),a(n,v),a(v,G),a(n,K),a(n,R),a(R,q),a(n,C),a(n,g),a(g,_),a(_,O),a(g,Q),a(g,E),a(E,W),a(g,X),a(g,S),a(S,Y),a(n,Z),a(n,h),a(h,$),a(h,b),a(b,aa),a(h,na)},p:ea,i:ea,o:ea,d(B){B&&l(n)}}}class ya extends ha{constructor(n){super(),pa(this,n,null,Pa,ba,{})}}export{ya as default};
---
+++ internal/immutable/pages/products/wpc.svelte-0b670261.js
@@ -0,0 +1,23 @@
+import{S as W,i as j,s as x,e as o,t as p,k as A,c as u,a as k,h as P,d as l,m as C,b as y,g as E,F as i,n as b}from"../../chunks/index-f1db066b.js";function T(D){let a,t,g,d,e,c,h,r,m;return{c(){a=o("div"),t=o("h2"),g=p("Kayu Asri"),d=A(),e=o("h3"),c=p("Wood Plastic Composite"),h=A(),r=o("p"),m=p(`Wood Plastic Composite adalah Solusi terbaik pengganti kayu, terbuat dari material yang terdiri dari komposisi 60% serbuk kayu jati, 30% Plastik HDPE Daur Ulang & 10% Bahan kimia yang ramah lingkungan. \r
+        Keunggulan WPC adalah sebagai berikut :\r
+        Anti Jamur\r
+        Anti Rayap\r
+        Tahan Air \r
+        Anti Oksidan\r
+        Anti UV (Ultra Violet)\r
+        Tidak menjalarkan api\r
+        Mudah dalam pemasangan\r
+        Bebas Perawatan\r
+        Tahan hingga puluhan tahun\r
+        Produk kami sangat cocok untuk bangunan yang banyak menggunakan material kayu (wood concept) karena dapat di aplikasikan sebagai Lantai (Decking), Dinding (Wall), Partisi/Penyekat ruangan (Partition), Plafon (Ceiling) serta dapat  dipakai sebagai material dekorasi untuk Pintu Pagar (Fencing).`),this.h()},l(s){a=u(s,"DIV",{class:!0});var n=k(a);t=u(n,"H2",{class:!0});var _=k(t);g=P(_,"Kayu Asri"),_.forEach(l),d=C(n),e=u(n,"H3",{class:!0});var v=k(e);c=P(v,"Wood Plastic Composite"),v.forEach(l),h=C(n),r=u(n,"P",{});var f=k(r);m=P(f,`Wood Plastic Composite adalah Solusi terbaik pengganti kayu, terbuat dari material yang terdiri dari komposisi 60% serbuk kayu jati, 30% Plastik HDPE Daur Ulang & 10% Bahan kimia yang ramah lingkungan. \r
+        Keunggulan WPC adalah sebagai berikut :\r
+        Anti Jamur\r
+        Anti Rayap\r
+        Tahan Air \r
+        Anti Oksidan\r
+        Anti UV (Ultra Violet)\r
+        Tidak menjalarkan api\r
+        Mudah dalam pemasangan\r
+        Bebas Perawatan\r
+        Tahan hingga puluhan tahun\r
+        Produk kami sangat cocok untuk bangunan yang banyak menggunakan material kayu (wood concept) karena dapat di aplikasikan sebagai Lantai (Decking), Dinding (Wall), Partisi/Penyekat ruangan (Partition), Plafon (Ceiling) serta dapat  dipakai sebagai material dekorasi untuk Pintu Pagar (Fencing).`),f.forEach(l),n.forEach(l),this.h()},h(){y(t,"class","svelte-2lkjae"),y(e,"class","svelte-2lkjae"),y(a,"class","container")},m(s,n){E(s,a,n),i(a,t),i(t,g),i(a,d),i(a,e),i(e,c),i(a,h),i(a,r),i(r,m)},p:b,i:b,o:b,d(s){s&&l(a)}}}class w extends W{constructor(a){super(),j(this,a,null,T,x,{})}}export{w as default};
