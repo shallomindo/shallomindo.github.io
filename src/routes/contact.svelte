284a82b96dd98326f528b317a71c7db440ee03c0
Ben Wu <benayaj.2004@gmail.com>
2022-06-23 11:49:19 +0700

    Add content

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,6 +1,22 @@
 <div class="container">
-    <h2>contact</h2>
-    <p><i class="fa-solid fa-phone"></i></p>
-    <p><a href="https://wa.me/628179170200"><i class="fa-brands fa-whatsapp"></i></a></p>
-    <p><a href="/contact">Facebook</a></p>
+    <h2>Contact Us</h2>
+    <div class="buttons">
+        <p><a href="tel:+62778472868"><i class="fa-solid fa-square-phone"></i> Telepon: +62 (778) 472 868</a></p>
+        <p><a href="https://wa.me/628179170200"><i class="fa-brands fa-whatsapp-square"></i> Hubungi kami melalui WhatsApp</a></p>
+        <p><a href="/contact"><i class="fa-brands fa-facebook-square"></i> Kunjungi Page Facebook</a></p>
+    </div>
 </div>
+
+<style lang="scss">
+    @use 'src/components/style/g';
+
+    .buttons {
+        @extend %flex-vertical;
+        justify-content: space-around;
+        align-items: center;
+        margin: 25px auto;
+        p {
+            margin: 10px auto;
+        }
+    }
+</style>
--- src/routes/products/bio2000.svelte
+++ src/routes/products/bio2000.svelte
@@ -1,4 +1,32 @@
 <div class="container">
     <h2>BIO 2000</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil minus quasi tempora. Placeat debitis eaque incidunt saepe, et veniam expedita dolorum dignissimos repellendus explicabo assumenda fuga officia aut quaerat recusandae!</p>
+    <p>
+        BIO 2000 adalah Formula Penguras WC hasil pengembangan bioteknologi yang dibuat dari bahan-bahan alami dengan bahan aktif 15% serbuk inokulan yang ramah lingkungan.
+        <br>
+        BIO 2000 berisi Bakteri organik dalam bentuk bubuk yang sangat efektif berguna sebagai pengurai limbah feses atau tinja untuk mencegah septik tank cepat penuh & mengatasi saluran yang mampat yang disebabkan oleh limbah organik serta menghilangkan bau tidak sedap tanpa merusak saluran/pipa dan ramah lingkungan.
+    </p>
+    <div class="box fungsi">
+        <p>
+            Fungsi BIO 2000 adalah sebagai berikut:
+        </p>
+        <ol>
+            <li><b>Mencegah WC penuh</b>, BIO 2000 merupakan starter bakteri pengurai (bifido bacteria) yang mempunyai kemampuan mendegradasi/mengurai limbah kotoran manusia, sehingga sangat baik digunakan untuk perawatan septic tank agar tidak cepat penuh.</li>
+            <li><b>Menghilangkan bau tidak sedap</b>, mampu menyerap bau-bauan.</li>
+            <li><b>Tidak merusak pipa saluran pembuangan</b>, aman bagi pipa karena tidak mengandung bahan kimia yang mengakibatkan korosi pada pipa saluran pembuangan.</li>
+            <li><b>Ramah lingkungan</b> karena terbuat dari bahan organik, sehingga aman bagi lingkungan, serta mampu mengurangi pencemaran lingkungan yang disebabkan oleh bakteri e-coli, karena mengandung bakteri baik (bakteri selulolitik, proteolitik dan amilolitik) yang dapat mematikan bakteri e-coli.</li>
+            <li><b>Praktis</b>, mudah dalam penggunaannya.</li>
+            <li><b>Hemat</b> karena lebih murah ketimbang biaya penyedotan tinja.</li>
+        </ol>
+    </div>
+    <div class="box pemakaian">
+        <p>
+            Cara penggunaannya sebagai berikut :
+        </p>
+        <ol>
+            <li>Masukkan 1 bungkus BIO 2000 ke dalam lobang WC atau kloset yang mampat, kemudian siram dengan air secukupnya sehingga bersih.</li>
+            <li>Hasil akan mulai terlihat setelah 3 - 5 jam dan volume tinja akan berkurang 75%, akan lebih effektif bila dibiarkan hingga semalaman.</li>
+            <li>Kloset dapat dipakai kembali seperti semula.</li>
+        </ol>
+    </div>
+    <p class="small">Tersedia dalam kemasan 500 gram</p>
 </div>
